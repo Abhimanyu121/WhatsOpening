e2400b9ee1dad02401e68cf3c1a93c88f4f6e4e2
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2020-05-10 00:29:56 +0530

    Nuked Background service for time being

--- android/app/src/main/kotlin/trellislabs/flu/MainActivity.kt
+++ android/app/src/main/kotlin/trellislabs/flu/MainActivity.kt
@@ -15,35 +15,35 @@ class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      // Create the NotificationChannel
-      val name = "Foam Maps"
-      val descriptionText = "Foam Map Location active"
-      val importance = NotificationManager.IMPORTANCE_DEFAULT
-      val mChannel = NotificationChannel("FOAM", name, importance)
-      mChannel.description = descriptionText
-      val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-      notificationManager.createNotificationChannel(mChannel)
-    }
-    val permissionAccessCoarseLocationApproved = ActivityCompat
-            .checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
-            PackageManager.PERMISSION_GRANTED
-    if (permissionAccessCoarseLocationApproved) {
-      startService()
-    } else {
-      // Make a request for foreground-only location access.
-      ActivityCompat.requestPermissions(this,
-              arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-              12
-      )
-      startService()
-    }
+//    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//      // Create the NotificationChannel
+//      val name = "Foam Maps"
+//      val descriptionText = "Foam Map Location active"
+//      val importance = NotificationManager.IMPORTANCE_DEFAULT
+//      val mChannel = NotificationChannel("FOAM", name, importance)
+//      mChannel.description = descriptionText
+//      val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+//      notificationManager.createNotificationChannel(mChannel)
+//    }
+//    val permissionAccessCoarseLocationApproved = ActivityCompat
+//            .checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+//            PackageManager.PERMISSION_GRANTED
+//    if (permissionAccessCoarseLocationApproved) {
+//      startService()
+//    } else {
+//      // Make a request for foreground-only location access.
+//      ActivityCompat.requestPermissions(this,
+//              arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+//              12
+//      )
+//      startService()
+//    }
 
   }
-  private fun startService(){
-    Intent(this, FoamService::class.java).also { intent ->
-      startService(intent)
-    }
-
-  }
+//  private fun startService(){
+//    Intent(this, FoamService::class.java).also { intent ->
+//      startService(intent)
+//    }
+//
+//  }
 }
