eec9a9ee944dc0f5beb12e9e177beba762214697
Abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2020-05-23 09:52:08 +0530

    working on matic wrapper

---
+++ lib/Wrappers/MaticWrapper.dart
@@ -0,0 +1,11 @@
+import 'dart:typed_data';
+
+import 'package:flutter/services.dart';
+import 'package:web3dart/crypto.dart';
+import 'package:web3dart/web3dart.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class MaticWrapper {
+  static const rpcUrl = "https://rinkeby.infura.io/v3/0e4ce57afbd04131b6842f08265b4d4b";
+}
